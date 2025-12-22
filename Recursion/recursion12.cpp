0829d205a95df6e71e49ccd0605442680112bbe9
Ayush Kumar <as1748846@gmail.com>
2025-12-23 01:52:27 +0530

    Added recursion12 program

---
+++ Recursion/recursion12.cpp
@@ -0,0 +1,21 @@
+//Check the given string is palindrome or not using recursion
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string s, int l, int r){
+    if(l>=r)
+       return true;
+    if(s[l]!=s[r])
+       return false;
+    return isPalindrome(s, l+1, r-1);
+}
+
+int main(){
+    string s;
+    cin>>s;
+    if(isPalindrome(s,0,s.length()-1))
+      cout<<"Palindrome";
+    else
+      cout<<"Not a Palindrome";
+    return 0;
+}
