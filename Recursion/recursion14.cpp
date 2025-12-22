fe1c991832b42d7611036ba972dcd45bdd1a65fb
Ayush Kumar <as1748846@gmail.com>
2025-12-23 02:43:05 +0530

    Added recursion14 program

---
+++ Recursion/recursion14.cpp
@@ -0,0 +1,18 @@
+//Multiple recursive calls to calculate the nth Fibonacci number
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    if(n==0)
+       return 0;
+    if(n==1)
+       return 1;
+    return fib(n-1)+fib(n-2);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fib(n);
+    return 0;
+}
