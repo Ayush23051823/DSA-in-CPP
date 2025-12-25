93bd83e419f55397fc360f1033b8509fc94aa3ca
Ayush Kumar <as1748846@gmail.com>
2025-12-25 23:00:28 +0530

    Added recursion16 program

---
+++ Recursion/recursion16.cpp
@@ -0,0 +1,16 @@
+//Sum of digits of a number
+#include<iostream>
+using namespace std;
+
+int sumofDigits(int n){
+    if(n==0)
+       return 0;
+    return(n%10)+sumofDigits(n/10);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<sumofDigits(n);
+    return 0;
+}
